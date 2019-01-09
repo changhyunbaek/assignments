f75e746a868dc6dbd66683b94f823c7520613b63
changhyunbaek <chbaek@gmail.com>
2019-01-09 09:57:36 -0500

    add functions to calculator

---
+++ calculator.py
@@ -0,0 +1,13 @@
+# This part of the code defines a function
+def multiply(a,b):
+    return a * b
+
+# This part of the code then calls that function and assigns the variable x to its return value
+x = multiply(8,9)
+
+# This prints x to the terminal for humans to read
+print(x)
+
+print("I'm going use the calculator functions to multiply 5 and 6")
+x = multiply(5,6)
+print(x)
